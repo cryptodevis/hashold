e4683ea395fdab6a37681c6000b97527a688fb5d
CryptoDevis <cryptodevis@gmail.com>
2017-07-05 01:28:05 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2811,7 +2811,7 @@ bool InitBlockIndex() {
         printf("%s\n", hash.ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x"));
+        assert(block.hashMerkleRoot == uint256("0x8402aed0f0c6a2ddd573cbd6c8204653c16d914472e2b825583c7da08b2ec7b3"));
         if (true && block.GetHash() != hashGenesisBlock)
             {
             printf("Searching for genesis block...\n");
