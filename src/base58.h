71b8b9ad167954c01d3266bd282744508a9b987f
CryptoDevis <cryptodevis@gmail.com>
2017-07-04 21:32:08 +0530

    Update base58.h

--- src/base58.h
+++ src/base58.h
@@ -272,9 +272,9 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 48, // Hashcoin addresses start with L
+        PUBKEY_ADDRESS = 41, // Hashcoin addresses start with H
         SCRIPT_ADDRESS = 5,
-        PUBKEY_ADDRESS_TEST = 111,
+        PUBKEY_ADDRESS_TEST = 100,
         SCRIPT_ADDRESS_TEST = 196,
     };
 
